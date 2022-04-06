4055378f418350faeb72b96c660a13de723c9bae
Humza Khan <83923770+HumzaKhan071@users.noreply.github.com>
2022-04-06 14:30:56 +0500

    Update settings.py

--- Myproject/settings.py
+++ Myproject/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-a^))o4i9vnq4%g4x*5p0j@@$w%hoxpxuvk0a^&(@30etq9r%+o'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'MyApp',
+    'MyApp.apps.MyappConfig',
     
 ]
 
